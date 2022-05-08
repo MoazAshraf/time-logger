5c88732708783db59841930bdcc54b1d97f13d52
Moaz Ashraf <mouadhashraf@gmail.com>
2022-05-08 17:00:21 +0200

    Calculate task duration from intervals

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+import util from "util";
 import http from "http";
 import express from "express";
 import logging from "morgan";
 import mongoose from "mongoose";
 import Task from "./models/task.js";
+import apiRouter from "./routes/api.js";
+
+// Promisify setTimeout
+const sleep = util.promisify(setTimeout);
 
 // HTTP Server
 const hostname = "localhost";
@@ -18,25 +23,43 @@ const db = await mongoose.connect(dbUrl);
 const app = express();
 app.use(logging);
 
-// TODO: Setup endpoint routes
+// Setup endpoint routes
+// app.get("/api/tasks", () => )
+app.use("/api", apiRouter);
 
 // Start the web server
 const server = http.createServer(app);
 server.listen(port, hostname, () => {
     console.log(`Server started on http://${hostname}:${port}`);
 
-    // Testing Task schema
-    const task = new Task({
-        name: "Task A",
-        intervals: [{ begin: new Date(), end: new Date() }],
-    });
-    task.save()
+    let begin = new Date();
+    const intervals = [];
+    sleep(100)
+        .then(() => {
+            intervals.push({ begin: begin, end: new Date() });
+            return sleep(100);
+        })
+        .then(() => {
+            begin = new Date();
+            return sleep(100);
+        })
+        .then(() => {
+            intervals.push({ begin: begin, end: new Date() });
+        })
+        .then(() => {
+            // Testing Task schema
+            const task = new Task({
+                name: "Task A",
+                intervals: intervals,
+            });
+            return task.save();
+        })
         .then((task) => {
             console.log(task);
             console.log(task.workDuration());
         })
         .then(() => {
-            Task.deleteMany().exec().then(console.log);
+            // Task.deleteMany().exec().then(console.log);
             // Task.find().exec().then(console.log);
         });
 });
--- models/task.js
+++ models/task.js
@@ -25,49 +25,10 @@ function getDuration(interval) {
 function getTotalDuration(intervals) {
     const durations = intervals.map((i) => getDuration(i));
     return durations.reduce((a, b) => {
-        console.log(a.add);
-        // TODO: correctly copy the timespan
-        a = { ...a };
-        console.log(a.add);
-        // a.add(b);
+        a.add(b);
         return a;
     }, new TimeSpan(0));
 }
-
-// function addIntervals(a, b) {
-//     const start = a.start;
-//     if (b.start < start) start = b.start;
-
-//     const end = a.end;
-//     if (end != null && (b.end == null || b.end > end)) end = b.end;
-//     return { start: start, end: end };
-// }
-
-// function sumIntervals(intervals) {
-//     if (intervals.length == 0) return null;
-//     if (intervals.length == 1) return { ...intervals[0] };
-//     let a = { ...intervals[0] };
-//     for (let i = 1; i < intervals.length; i++) {
-//         b = intervals[i];
-//         a = addIntervals(a, b);
-//     }
-//     return a;
-// }
-
-// function addDuration(a, b) {
-//     if (a.end == null || b.end == null) return null;
-
-//     a.d;
-
-//     const start = a.start;
-//     if (b.start < start) start = b.start;
-
-//     const end = a.end;
-//     if (end != null && (b.end == null || b.end > end)) end = b.end;
-//     return { start: start, end: end };
-// }
-
-// function sumDuration(intervals) {}
 
 const taskSchema = new mongoose.Schema({
     name: {
