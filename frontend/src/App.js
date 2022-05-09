d22d86df1ecbdcbf44eecfb647e04480684a4041
Moaz Ashraf <mouadhashraf@gmail.com>
2022-05-09 21:25:44 +0200

    Frontend: fetch & render tasks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,38 @@
+import { Component, useState } from "react";
 import "./App.css";
 
-function App() {
-    fetch("http://localhost:8000/api")
-        .then((res) => res.text())
-        .then(console.log);
+const apiUrl = "http://localhost:8000/api";
 
-    return (
-        <div className="App">
-            <h1>Time Logger</h1>
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { tasks: [] };
+    }
+
+    componentDidMount() {
+        console.log(this.state.tasks);
+        if (this.state.tasks.length === 0) {
+            fetch(`${apiUrl}/tasks`)
+                .then((res) => res.json())
+                .then((tasks) => {
+                    console.log(tasks);
+                    this.setState({tasks: tasks})
+                });
+        }
+    }
+
+    render() {
+        const tasksHtml = this.state.tasks.map((task) => (
+            <li key={task._id}>{task.name}</li>
+        ));
+
+        return (
+            <div className="App">
+                <h1>Time Logger</h1>
+                <ul className="tasks">{tasksHtml}</ul>
+            </div>
+        );
+    }
 }
 
 export default App;
